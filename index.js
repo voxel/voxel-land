ed95bae12bc5a31178df8a6824b6e9e3d6fbf6d8
deathcap <deathcap@gmx.co.uk>
2014-03-23 20:26:20 -0700

    Add creativeTab for voxel-inventory-creative
    Ref https://github.com/deathcap/voxel-ideas/issues/19

--- index.js
+++ index.js
@@ -76,18 +76,19 @@ Land.prototype.registerBlocks = function()  {
     this.registry.registerBlock('grass', {texture: ['grass_top', 'dirt', 'grass_side'], hardness:1.0, itemDrop: 'dirt', effectiveTool: 'spade'});
     this.registry.registerBlock('dirt', {texture: 'dirt', hardness:0.75, effectiveTool: 'spade'});
     this.registry.registerBlock('stone', {texture: 'stone', hardness:10, itemDrop: 'cobblestone', effectiveTool: 'pickaxe'});
-    this.registry.registerBlock('logOak', {displayName: 'Oak Wood', texture: ['log_oak_top', 'log_oak_top', 'log_oak'], hardness:2.0, effectiveTool: 'axe'});
+    this.registry.registerBlock('logOak', {displayName: 'Oak Wood', texture: ['log_oak_top', 'log_oak_top', 'log_oak'], hardness:2.0, effectiveTool: 'axe', creativeTab: 'plants'});
     this.registry.registerBlock('cobblestone', {texture: 'cobblestone', hardness:10.0});
     this.registry.registerBlock('oreCoal', {displayName: 'Coal Ore', texture: 'coal_ore', itemDrop: 'coal', hardness:15.0});
     this.registry.registerBlock('oreIron', {displayName: 'Iron Ore', texture: 'iron_ore', hardness:15.0});
     this.registry.registerBlock('brick', {texture: 'brick'}); // some of the these blocks don't really belong here..do they?
     this.registry.registerBlock('obsidian', {texture: 'obsidian', hardness: 128});
-    this.registry.registerBlock('leavesOak', {displayName: 'Oak Leaves', texture: 'leaves_oak', hardness: 0.1,
+    this.registry.registerBlock('leavesOak', {displayName: 'Oak Leaves', texture: 'leaves_oak', hardness: 0.1, creativeTab: 'plants',
       // if voxel-food apple is enabled, drop it when breaking laves (oak apples)
       itemDrop: this.registry.getItemProps('apple') ? 'apple' : null});
     this.registry.registerBlock('glass', {texture: 'glass', hardness: 0.2});
 
-    this.registry.registerBlock('logBirch', {texture: ['log_birch_top', 'log_birch_top', 'log_birch'], hardness:2.0, displayName: 'Birch Wood', effectiveTool: 'axe'}); // TODO: generate
+    this.registry.registerBlock('logBirch', {texture: ['log_birch_top', 'log_birch_top', 'log_birch'], hardness:2.0,
+      displayName: 'Birch Wood', effectiveTool: 'axe', creativeTab: 'plants'}); // TODO: generate
   }
 
   if (this.opts.registerRecipes) {
