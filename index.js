c04f268416e109719f094c0386ebfb60983009ca
deathcap <deathcap@gmx.co.uk>
2014-05-02 17:57:13 -0700

    Require pickaxes to mine stone and ores
    Requires voxel-harvest 0.3.0+, deathcap/voxel-harvest@ce1efbf36d752f70be9c8de34802ad9f4c102dc7

--- index.js
+++ index.js
@@ -75,13 +75,13 @@ Land.prototype.registerBlocks = function()  {
   if (this.opts.registerBlocks) {
     this.registry.registerBlock('grass', {texture: ['grass_top', 'dirt', 'grass_side'], hardness:1.0, itemDrop: 'dirt', effectiveTool: 'spade'});
     this.registry.registerBlock('dirt', {texture: 'dirt', hardness:0.75, effectiveTool: 'spade'});
-    this.registry.registerBlock('stone', {texture: 'stone', hardness:10, itemDrop: 'cobblestone', effectiveTool: 'pickaxe'});
+    this.registry.registerBlock('stone', {texture: 'stone', hardness:10.0, itemDrop: 'cobblestone', effectiveTool: 'pickaxe', requiredTool: 'pickaxe'});
     this.registry.registerBlock('logOak', {displayName: 'Oak Wood', texture: ['log_oak_top', 'log_oak_top', 'log_oak'], hardness:2.0, effectiveTool: 'axe', creativeTab: 'plants'});
-    this.registry.registerBlock('cobblestone', {texture: 'cobblestone', hardness:10.0});
-    this.registry.registerBlock('oreCoal', {displayName: 'Coal Ore', texture: 'coal_ore', itemDrop: 'coal', hardness:15.0});
-    this.registry.registerBlock('oreIron', {displayName: 'Iron Ore', texture: 'iron_ore', hardness:15.0});
+    this.registry.registerBlock('cobblestone', {texture: 'cobblestone', hardness:10.0, requiredTool: 'pickaxe'});
+    this.registry.registerBlock('oreCoal', {displayName: 'Coal Ore', texture: 'coal_ore', itemDrop: 'coal', hardness:15.0, requiredTool: 'pickaxe'});
+    this.registry.registerBlock('oreIron', {displayName: 'Iron Ore', texture: 'iron_ore', hardness:15.0, requiredTool: 'pickaxe'});
     this.registry.registerBlock('brick', {texture: 'brick'}); // some of the these blocks don't really belong here..do they?
-    this.registry.registerBlock('obsidian', {texture: 'obsidian', hardness: 128});
+    this.registry.registerBlock('obsidian', {texture: 'obsidian', hardness: 128, requiredTool: 'pickaxe'});
     this.registry.registerBlock('leavesOak', {displayName: 'Oak Leaves', texture: 'leaves_oak_opaque', hardness: 0.1, creativeTab: 'plants',
       // if voxel-food apple is enabled, drop it when breaking laves (oak apples)
       itemDrop: this.registry.getItemProps('apple') ? 'apple' : null});
